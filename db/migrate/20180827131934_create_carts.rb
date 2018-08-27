a3a49c0a15043d218b44980826eae21b2dff7450
ldelestre <l98.broadhurst@gmail.com>
2018-08-27 16:58:08 +0200

    Dare

--- db/migrate/20180827131934_create_carts.rb
+++ db/migrate/20180827131934_create_carts.rb
@@ -1,7 +1,7 @@
 class CreateCarts < ActiveRecord::Migration[5.2]
   def change
     create_table :carts do |t|
-
+	  t.belongs_to :user, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20180827140752_create_orders.rb
+++ db/migrate/20180827140752_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.belongs_to :user, foreign_key: true
+      t.belongs_to :cart, foreign_key: true
       t.timestamps
     end
   end
