0b37144391ca076a4117f3af9d2481638cc0de29
ldelestre <l98.broadhurst@gmail.com>
2018-08-27 15:26:13 +0200

    Dare

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ApplicationRecord
 	validates :description, presence: true
 	validates :price, presence: true
 	validates :image_url, presence: true
+	has_and_belongs_to_many :carts
 end
