809accc4f140cdcc7a9b129ca6d0e6146fff9169
Ridkuruma <ridwan_92@yahoo.fr>
2018-08-28 17:49:01 +0200

    controller item

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'items/listing'
   devise_for :users
   get 'home/index'
   root 'home#index'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Item.create(title: 'Nike Blazer Mid', description: 'La description est en maintenance', price: '100', image_url:'https://www.stadiumgoods.com/media/catalog/product/cache/1/base/1000x600/9df78eab33525d08d6e5fb8d27136e95/a/a/aa3832_100_3_1.png')
+Item.create(title: 'Air Jordan 1', description: 'La description est en maintenance', price: '100', image_url:'https://www.stadiumgoods.com/media/catalog/product/cache/1/base/1000x600/9df78eab33525d08d6e5fb8d27136e95/a/a/aa3834_101_2_2.png')
+Item.create(title: 'Nike Air Presto', description: 'La description est en maintenance', price: '100', image_url:'https://www.stadiumgoods.com/media/catalog/product/cache/1/base/1000x600/9df78eab33525d08d6e5fb8d27136e95/a/a/aa3830_001_3_3.png')
+Item.create(title: 'Nike Air Vapormax', description: 'La description est en maintenance', price: '100', image_url:'https://www.stadiumgoods.com/media/catalog/product/cache/1/base/1000x600/9df78eab33525d08d6e5fb8d27136e95/a/a/aa3831_001_4.png')
+Item.create(title: 'Nike Air Max 90', description: 'La description est en maintenance', price: '100', image_url:'https://www.stadiumgoods.com/media/catalog/product/cache/1/base/1000x600/9df78eab33525d08d6e5fb8d27136e95/a/a/aa7293_100_2_2.png')
+Item.create(title: 'Nike Air Max 97', description: 'La description est en maintenance', price: '100', image_url:'https://www.stadiumgoods.com/media/catalog/product/cache/1/base/1000x600/9df78eab33525d08d6e5fb8d27136e95/A/J/AJ4585-100_2.png')
+Item.create(title: 'Nike Air Force 1 Low', description: 'La description est en maintenance', price: '100', image_url:'https://www.stadiumgoods.com/media/catalog/product/cache/1/base/1000x600/9df78eab33525d08d6e5fb8d27136e95/a/o/ao4606_100_2_1.png')
+Item.create(title: 'Nike Zoom Fly', description: 'La description est en maintenance', price: '100', image_url:'https://www.stadiumgoods.com/media/catalog/product/cache/1/base/1000x600/9df78eab33525d08d6e5fb8d27136e95/a/j/aj4588_100_2_3.png')
