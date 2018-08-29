bd1492b0ae9bf054956b0190723daadb6fc3b8b7
Lvmmbrkln <j.degueltzl@gmail.com>
2018-08-29 22:31:07 +0200

    debut cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
 
-before_action :sanitize_devise_params, if: :devise_controller?
+	protect_from_forgery with: :exception
+	before_action :set_cart
 
-	def sanitize_devise_params
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
-	end
+	private
+		def set_cart
+			@cart = Cart.find(session[:cart_id])
+		rescue ActiveRecord::RecordNotFound
+			@cart = Cart.create
+			session[:cart_id] = @cart.id
+		end
+	before_action :sanitize_devise_params, if: :devise_controller?
+
+		def sanitize_devise_params
+			devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
+		end
 
 end
