05249a4fb210de6af7f67855bf0b556048b71ae1
Ridkuruma <ridwan_92@yahoo.fr>
2018-08-29 17:02:25 +0200

    clean login sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,13 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'items/all', to: 'items#listing'
-  devise_for :users
-  get 'home/index'
-  root 'home#index'
-  resources :charges
-  get '/account', to: 'home#account'
-=======
 
-	devise_for :users
-	resources :charges
-
-	root 'home#index'
-
-	get '/cart', to: 'carts#cart'
-	get '/items/all', to: 'items#listing'
-	get '/home/index'
-
->>>>>>> a8f7a204bbd294e54714338aa66618c856a279f9
+	  get 'items/all', to: 'items#listing'
+	  devise_for :users
+	  get 'home/index'
+	  root 'home#index'
+	  resources :charges
+	  get '/account', to: 'home#account'
+	  get '/cart', to: 'carts#cart'
+	  get '/items/all', to: 'items#listing'
+	  
 end
+
