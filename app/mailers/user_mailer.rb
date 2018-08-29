004fe6fd124ac98a98430b28b5d633a0badeb593
Nicolas Hermet <n.hermet@gmail.com>
2018-08-29 17:13:07 +0200

    adding variable in order mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
-  def order_email(price, recipient)
-     mail(from: "dumy.fool@gmail.com", to: recipient,
-          subject: "Confirmation de votre commande de #{price}€")
+  def order_email(price, currency, recipient)
+     mail(from: "dumy.fool@gmail.com", name: 'Mailjet Pilot', to: recipient,
+     	  variables: [price, currency, recipient],
+          subject: "Confirmation de votre commande de #{price.to_s+currency.to_s}€")
    end
 end
