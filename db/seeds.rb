60fc7384b2a57771db02b2f24f4af1472f6077ed
Nicolas Hermet <n.hermet@gmail.com>
2018-08-29 08:54:33 +0200

    merge fix from 809accc4f140cdcc7a9b129ca6d0e6146fff9169

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-<<<<<<< HEAD
+
 Item.destroy_all
 
-=======
->>>>>>> 809accc4f140cdcc7a9b129ca6d0e6146fff9169
 Item.create(title: 'Nike Blazer Mid', description: 'La description est en maintenance', price: '100', image_url:'https://www.stadiumgoods.com/media/catalog/product/cache/1/base/1000x600/9df78eab33525d08d6e5fb8d27136e95/a/a/aa3832_100_3_1.png')
 Item.create(title: 'Air Jordan 1', description: 'La description est en maintenance', price: '100', image_url:'https://www.stadiumgoods.com/media/catalog/product/cache/1/base/1000x600/9df78eab33525d08d6e5fb8d27136e95/a/a/aa3834_101_2_2.png')
 Item.create(title: 'Nike Air Presto', description: 'La description est en maintenance', price: '100', image_url:'https://www.stadiumgoods.com/media/catalog/product/cache/1/base/1000x600/9df78eab33525d08d6e5fb8d27136e95/a/a/aa3830_001_3_3.png')
