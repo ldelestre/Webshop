07ef246300b6864b3a9b36295c8ceeecee53aaa8
ldelestre <l98.broadhurst@gmail.com>
2018-08-30 18:21:16 +0200

    account added to routes + link to account added in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
 	get '/about', to: 'home#about'
 	get '/contact', to: 'home#contact'
 	get '/services', to: 'home#services'
+	get '/account', to: 'home#account'
 end
