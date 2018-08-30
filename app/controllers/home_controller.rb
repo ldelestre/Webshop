3effd7c7b1abeccccf6d0949bcd10832688f14e9
Nicolas Hermet <n.hermet@gmail.com>
2018-08-30 20:39:17 +0200

    add user account dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,13 +3,18 @@ class HomeController < ApplicationController
   	@items = Item.all
   end
   def account
-    @orders = Order.all
+    @arr = []
+    @orders = Order.where(user: current_user)
+    @orders.each do |o|
+      @arr << o.date
+    end
+    @arr.uniq
   end
   def single
   	@item = Item.find_by(id: params[:id])
   end
 
   def product
-  	@items = Item.all
+  	@items = Item.where(user: current_user)
   end
 end
