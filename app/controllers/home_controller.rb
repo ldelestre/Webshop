73f4bd368ec41cbca4d36d5a3638e8f1e3f9fc05
Nicolas Hermet <n.hermet@gmail.com>
2018-08-30 20:59:11 +0200

    fix bug on Sneaker access for not logged-in users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :single, :product, :services, :about, :contact]
   def index
   	@items = Item.all
   end
